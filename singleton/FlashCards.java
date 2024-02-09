05e8f2dee7ac30f4a461c3110f8e038d4a6eda1a
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-02-09 17:14:56 -0500

    Added javadoc to singleton

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,25 +1,40 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * A singleton collection of flash cards
+ * @author Isaac Morris
+ */
 public class FlashCards {
     private Random rand;
     private static FlashCards flashCards;
     private ArrayList<Word> words;
     private Word currentWord;
 
+    /**
+     * Creates a FlashCards instance (once)
+     */
     private FlashCards(){
         this.rand = new Random();
         this.words = FileReader.getWords();
         this.currentWord = this.words.get(rand.nextInt(words.size()));
     }
 
+    /**
+     * Returns this instance of FlashCards, creating the instance if not 
+     * constructed yet
+     * @return this FlashCards
+     */
     public static FlashCards getInstance(){
         if(flashCards == null){
             flashCards = new FlashCards();
         }
         return flashCards;
     }
-
+    /**
+     * Returns a new random word from the words list
+     * @return a random word
+     */
     public Word getWord(){
         Word word;
         do{
--- singleton/Word.java
+++ singleton/Word.java
@@ -1,21 +1,42 @@
 
+/**
+ * A word with info about the word
+ * @author Isaac Morris
+ */
 public class Word {
     private String word;
     private String type;
     private String definition;
     private String sentence;
 
+    /**
+     * Constructs a new word with the given arguments
+     * @param word String representation of the word
+     * @param type Part of speech of the word
+     * @param definition Definition of the word
+     * @param sentence The word used in a sentence
+     */
     public Word(String word, String type, String definition, String sentence){
         this.word = word;
         this.type = type;
         this.definition = definition;
         this.sentence = sentence;
     }
-
+    /**
+     * Returns the String version of this word
+     * @return This word as a String
+     */
     public String getWord(){
         return word;
     }
-
+    /**
+     * Returns the part of speech and definition of this word, as well as it
+     * used in a sentence
+     * @return The description of this word with the form:
+     * Part of Speech: <type>
+     * Definition: <definition>
+     * <sentence>
+     */
     public String getDescription(){
         String description = "";
         description += "Part of Speech: " + type + "\n";
